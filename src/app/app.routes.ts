ad9c664bbb5396b84b14729078af6c9a375a1687
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-02-28 16:50:33 -0500

    feat: add default path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { CatalogComponent } from './components/catalog/catalog.component';
 
 export const routes: Routes = [
     {
+        path: '', redirectTo: '/catalog', pathMatch: 'full'
+    },
+    {
         path:'cart',
         component: CartComponent
     },
