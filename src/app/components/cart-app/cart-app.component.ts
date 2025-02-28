44fa556723d09a65268faa46ee38af46b68a7662
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-02-28 13:45:35 -0500

    feat: add ngOnChanges validation and delete comments

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -15,7 +15,6 @@ import { CartModalComponent } from '../cart-modal/cart-modal.component';
 export class CartAppComponent implements OnInit{
   products: Product[] = [];
   cartItems: CartItem[] = [];
-  // total: number = 0;
   showCart: boolean = false;
 
   constructor(private product_service: ProductService){
@@ -25,7 +24,6 @@ export class CartAppComponent implements OnInit{
   ngOnInit(): void {
     this.products = this.product_service.findAll();
     this.cartItems = JSON.parse(sessionStorage.getItem('cartItems')!) || [];
-    // this.calculateTotal();
   }
 
   onAddToCart(product: Product){
@@ -41,8 +39,6 @@ export class CartAppComponent implements OnInit{
     }else{
       this.cartItems = [...this.cartItems, {product:{...product}, quantity:1}];
     }
-    // this.calculateTotal();
-    // this.saveSession();
   }
 
   onDeleteCartItem(id: number){
@@ -57,8 +53,9 @@ export class CartAppComponent implements OnInit{
     }else{
       this.cartItems = this.cartItems.filter(cartItem => cartItem.product.id !== id);
     }
-    // this.calculateTotal();
-    // this.saveSession();
+    if (this.cartItems.length === 0){
+      sessionStorage.removeItem('cartItems');
+    }
   }
 
   onShowCart(){
