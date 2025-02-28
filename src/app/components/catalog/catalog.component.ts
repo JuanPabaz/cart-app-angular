c23be25d10c8cf7a0b242af25399e2659952afdc
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-02-28 16:20:15 -0500

    feat: make catalog its own component

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { Router } from '@angular/router';
+import { SharingDataService } from '../../services/sharing-data.service';
 
 @Component({
   selector: 'app-catalog',
@@ -9,10 +11,16 @@ import { ProductCardComponent } from '../product-card/product-card.component';
   styleUrl: './catalog.component.css'
 })
 export class CatalogComponent {
-  @Input() products: Product[] = [];
-
-  @Output() addToCartEventEmitter: EventEmitter<Product> = new EventEmitter();
-  onAddToCart(product: Product){
-    this.addToCartEventEmitter.emit(product)
+  products: Product[] = [];
+  
+  constructor(private router: Router,
+    private sharing_data_service: SharingDataService
+  ){
+      this.products = this.router.getCurrentNavigation()?.extras.state!['products'];
   }
+
+  onAddToCart(product: Product){
+    this.sharing_data_service.addToCartEventEmitter.emit(product)
+  }
+
 }
