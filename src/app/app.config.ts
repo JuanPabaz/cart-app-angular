279ce5dece40e68f344ab6f8f7685f31a37ce644
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-02 18:49:26 -0500

    feat: add ngrx devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { cartItemsReducer } from './store/items.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    provideClientHydration(withEventReplay()), 
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
     provideStore({
-      items: cartItemsReducer
-    })]
+        items: cartItemsReducer
+    }), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
