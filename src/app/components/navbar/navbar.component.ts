6f528d7e0f4879f6405f6d326235689b61658e99
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-02 18:40:35 -0500

    feat: get cartItems via items store

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CartItem } from '../../models/cartItem';
 import { Product } from '../../models/product';
+import { Store } from '@ngrx/store';
+import { itemsState } from '../../store/items.reducer';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +12,11 @@ import { Product } from '../../models/product';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  @Input() cartItems: CartItem[] = [];
-  @Input() total: number = 0;
+  cartItems: CartItem[] = [];
+
+  constructor(private store: Store<{items: itemsState}>){
+    store.select('items').subscribe(state => {
+      this.cartItems = state.items
+    })
+  }
 }
